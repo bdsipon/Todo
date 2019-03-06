2f842667589fb5f6a9b23375d5ad512f07d534c7
bdsipon <sipon.here@gmail.com>
2019-03-06 16:15:39 +1000

    Final touches

--- TodoList/src/app/todolist/todolist.component.ts
+++ TodoList/src/app/todolist/todolist.component.ts
@@ -5,11 +5,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css']
 })
-export class TodolistComponent implements OnInit {
+export class TodolistComponent  {
 
-  constructor() { }
+  newTodo: string;
+  todos: any;
+  todoObj: any;
 
-  ngOnInit() {
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
+  }
+
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    }
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  deleteSelectedTodos() {
+    //need ES5 to reverse loop in order to splice by index
+    for(var i=(this.todos.length -1); i > -1; i--) {
+      if(this.todos[i].completed) {
+        this.todos.splice(i, 1);
+      }
+    }
   }
 
 }
